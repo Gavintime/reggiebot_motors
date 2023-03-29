384126c5c078f4ebd812080dba5607b791dc1a4f
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-03-28 18:26:32 -0700

    Add motor speed getters and setters

--- motor_controller/motor_driver.h
+++ motor_controller/motor_driver.h
@@ -5,17 +5,26 @@ namespace MotorDriver
 {
     // L298N motor driver connections
     // Motor A
-    constexpr uint8_t left_motor_pin_speed{6};
-    constexpr uint8_t left_motor_pin_forward{7};
-    constexpr uint8_t left_motor_pin_backward{8};
+    constexpr uint8_t left_motor_speed_pin{6};
+    constexpr uint8_t left_motor_forward_pin{7};
+    constexpr uint8_t left_motor_backward_pin{8};
     // Motor B
-    constexpr uint8_t right_motor_pin_speed{11};
-    constexpr uint8_t right_motor_pin_forward{9};
-    constexpr uint8_t right_motor_pin_backward{10};
+    constexpr uint8_t right_motor_speed_pin{11};
+    constexpr uint8_t right_motor_forward_pin{9};
+    constexpr uint8_t right_motor_backward_pin{10};
+
+    enum MotorSelection
+    {
+        LEFT,
+        RIGHT
+    };
 
     void initMotors();
     // motors may stall below 20% power, confirmed stalling at 10% power
-    int8_t setMotorSpeeds(int8_t leftMotorSpeed, int8_t rightMotorSpeed);
+    int8_t setMotorSpeed(MotorSelection motor, int8_t motor_speed);
+    int8_t setMotorSpeeds(int8_t left_motor_speed, int8_t right_motor_speed);
+    int8_t getMotorSpeed(MotorSelection motor);
     void stopMotors();
+
 }
 #endif
--- motor_controller/motor_driver.ino
+++ motor_controller/motor_driver.ino
@@ -1,86 +1,101 @@
 #include "motor_driver.h"
 
+// between -100 and 100
+static int8_t _left_speed{0};
+static int8_t _right_speed{0};
+
 // initialize motor pins and turn motors off
 void MotorDriver::initMotors()
 {
     // Set all the motor control pins to outputs
-    pinMode(left_motor_pin_speed, OUTPUT);
-    pinMode(left_motor_pin_forward, OUTPUT);
-    pinMode(left_motor_pin_backward, OUTPUT);
+    pinMode(left_motor_speed_pin, OUTPUT);
+    pinMode(left_motor_forward_pin, OUTPUT);
+    pinMode(left_motor_backward_pin, OUTPUT);
 
-    pinMode(right_motor_pin_speed, OUTPUT);
-    pinMode(right_motor_pin_forward, OUTPUT);
-    pinMode(right_motor_pin_backward, OUTPUT);
+    pinMode(right_motor_speed_pin, OUTPUT);
+    pinMode(right_motor_forward_pin, OUTPUT);
+    pinMode(right_motor_backward_pin, OUTPUT);
 
     // default motors to off
     stopMotors();
 }
 
-// takes in 2 motor speeds between -100 and 100, and sets motor pins accordingly
-// error code -1 = a motor speed was outside the range of -100 to 100
+// takes in a motor speed between -100 and 100, and sets motor pins accordingly
+// error code -1: a motor speed was outside the range of -100 to 100
+// error code -2: invalid motor selection
 // TODO: how to deal with minimum real speeds (before stalling)
-int8_t MotorDriver::setMotorSpeeds(int8_t leftMotorSpeed, int8_t rightMotorSpeed)
+int8_t MotorDriver::setMotorSpeed(MotorSelection motor, int8_t motor_speed)
 {
-    bool leftReverese{false};
-    bool rightReverse{false};
-
-    if (leftMotorSpeed < 0)
-    {
-        leftMotorSpeed *= -1;
-        leftReverese = true;
-    }
-
-    if (rightMotorSpeed < 0)
-    {
-        rightMotorSpeed *= -1;
-        rightReverse = true;
-    }
-
     // speed requests must be between -100 and 100
-    if (leftMotorSpeed > 100 || rightMotorSpeed > 100)
+    if (motor_speed > 100)
     {
         return -1;
     }
 
-    // map speeds from 0-100 to 0-255
-    leftMotorSpeed = map(leftMotorSpeed, 0, 100, 0, 255);
-    rightMotorSpeed = map(rightMotorSpeed, 0, 100, 0, 255);
+    // get pins of selected motor and set global speed
+    uint8_t speed_pin;
+    uint8_t forward_pin;
+    uint8_t backward_pin;
+    switch (motor)
+    {
+    case LEFT:
+        speed_pin = left_motor_speed_pin;
+        forward_pin = left_motor_forward_pin;
+        backward_pin = left_motor_backward_pin;
+        _left_speed = motor_speed;
+        break;
+    case RIGHT:
+        speed_pin = right_motor_speed_pin;
+        forward_pin = right_motor_forward_pin;
+        backward_pin = right_motor_backward_pin;
+        _right_speed = motor_speed;
+        break;
+    // impossible motor selection
+    default:
+        return -2;
+    }
 
-    // set motor speed and direction
-    analogWrite(left_motor_pin_speed, leftMotorSpeed);
-    analogWrite(right_motor_pin_speed, rightMotorSpeed);
-
-    if (!leftReverese)
+    // if reverse speed, swap pins and make speed absolute
+    if (motor_speed < 0)
     {
-        digitalWrite(left_motor_pin_forward, HIGH);
-        digitalWrite(left_motor_pin_backward, LOW);
-    }
-    else
-    {
-        digitalWrite(left_motor_pin_forward, LOW);
-        digitalWrite(left_motor_pin_backward, HIGH);
+        motor_speed *= -1;
+        auto temp{forward_pin};
+        forward_pin = backward_pin;
+        backward_pin = temp;
     }
 
-    if (!rightReverse)
-    {
-        digitalWrite(right_motor_pin_forward, HIGH);
-        digitalWrite(right_motor_pin_backward, LOW);
-    }
-    else
-    {
-        digitalWrite(right_motor_pin_forward, LOW);
-        digitalWrite(right_motor_pin_backward, HIGH);
-    }
+    analogWrite(speed_pin, map(motor_speed, 0, 100, 0, 255));
+    digitalWrite(forward_pin, HIGH);
+    digitalWrite(backward_pin, LOW);
 
     return 0;
 }
 
+int8_t MotorDriver::setMotorSpeeds(int8_t left_motor_speed, int8_t right_motor_speed)
+{
+    setMotorSpeed(LEFT, left_motor_speed);
+    setMotorSpeed(RIGHT, right_motor_speed);
+}
+
+int8_t MotorDriver::getMotorSpeed(MotorSelection motor)
+{
+    switch (motor)
+    {
+        case LEFT:
+            return _left_speed;
+        case RIGHT:
+            return _right_speed;
+        default:
+            return -2;
+    }
+}
+
 void MotorDriver::stopMotors()
 {
-    analogWrite(left_motor_pin_speed, 0);
-    analogWrite(right_motor_pin_speed, 0);
-    digitalWrite(left_motor_pin_forward, LOW);
-    digitalWrite(left_motor_pin_backward, LOW);
-    digitalWrite(right_motor_pin_forward, LOW);
-    digitalWrite(right_motor_pin_backward, LOW);
+    analogWrite(left_motor_speed_pin, 0);
+    analogWrite(right_motor_speed_pin, 0);
+    digitalWrite(left_motor_forward_pin, LOW);
+    digitalWrite(left_motor_backward_pin, LOW);
+    digitalWrite(right_motor_forward_pin, LOW);
+    digitalWrite(right_motor_backward_pin, LOW);
 }
