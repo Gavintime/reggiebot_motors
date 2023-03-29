0d8adc9f3ff0b2f3b1caff0c15fcba2b1e849e3f
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-03-29 13:54:28 -0700

    correct encoder_ppr value

--- motor_controller/encoder.h
+++ motor_controller/encoder.h
@@ -6,7 +6,7 @@ namespace Encoder
     // all lengths are in millimeters except counts per meter
 
     // TODO: 75:1 gear ratio, 11 ppr (spec sheet), 75*11=825
-    constexpr auto encoder_ppr{814};
+    constexpr auto encoder_ppr{825};
     // TODO: measure this
     constexpr auto wheel_gap{200}; // guesstimated 8 inches for now
     constexpr double wheel_circumference{204.2}; // 65mm diameter * pi
