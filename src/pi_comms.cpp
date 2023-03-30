ac46462e342a0800c50fd3117b33d23f5d420611
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-03-30 16:35:04 -0700

    Add basic serial communication code

---
+++ src/pi_comms.cpp
@@ -0,0 +1,37 @@
+#include <libserial/SerialPort.h>
+#include <libserial/SerialPortConstants.h>
+#include <iostream>
+#include <string>
+
+std::string getCommandFromUser()
+{
+    std::string str;
+    std::cout << "> ";
+    getline(std::cin, str);
+    return str;
+}
+
+int main()
+{
+    // connect to arduino over serial port and set serial parameters
+    LibSerial::SerialPort arduino_port("/dev/ttyACM0");
+    arduino_port.SetBaudRate(LibSerial::BaudRate::BAUD_9600);
+    arduino_port.SetCharacterSize(LibSerial::CharacterSize::CHAR_SIZE_8);
+    arduino_port.SetFlowControl(LibSerial::FlowControl::FLOW_CONTROL_NONE);
+    arduino_port.SetParity(LibSerial::Parity::PARITY_NONE);
+    arduino_port.SetStopBits(LibSerial::StopBits::STOP_BITS_1);
+
+    while (true)
+    {
+        std::string command{getCommandFromUser()};
+        arduino_port.Write(command);
+
+        // get response if get encoder count or get motor speed command
+        if (command[0] == 'r' || command[0] == 'g')
+        {
+            std::string response;
+            arduino_port.ReadLine(response, '\n');
+            std::cout << response;
+        }
+    }
+}
