06744b9f74a2a8d9784d7b01c504e14061d923f4
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-04-21 18:09:10 -0700

    make sending and receiving have their own threads

--- src/pi_comms.cpp
+++ src/pi_comms.cpp
@@ -2,13 +2,25 @@
 #include <libserial/SerialPortConstants.h>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <functional>
 
 std::string getCommandFromUser()
 {
     std::string str;
-    std::cout << "> ";
+    // std::cout << "> ";
     getline(std::cin, str);
     return str;
+}
+
+void printArduinoOutput(LibSerial::SerialPort &arduino_port)
+{
+    while (true)
+    {
+        std::string response;
+        arduino_port.ReadLine(response, '\n');
+        std::cout << response;
+    }
 }
 
 int main()
@@ -21,17 +33,23 @@ int main()
     arduino_port.SetParity(LibSerial::Parity::PARITY_NONE);
     arduino_port.SetStopBits(LibSerial::StopBits::STOP_BITS_1);
 
+    std::thread printer{printArduinoOutput, std::ref(arduino_port)};
+
     while (true)
     {
         std::string command{getCommandFromUser()};
         arduino_port.Write(command + '\n');
 
         // get response if get encoder count or get motor speed command
-        if (command[0] == 'r' || command[0] == 'g' || command[0] == 'o')
+        // if (command[0] == 'r' || command[0] == 'g' || command[0] == 'o')
+        // {
+        // std::string response;
+        // arduino_port.ReadLine(response, '\n');
+        // std::cout << response;
+        // }
+        if (command[0] == 'p')
         {
-            std::string response;
-            arduino_port.ReadLine(response, '\n');
-            std::cout << response;
+            arduino_port.Close();
         }
     }
 }
